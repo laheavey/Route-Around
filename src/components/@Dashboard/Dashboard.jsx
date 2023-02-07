1013c23b1ca2ee6664a2dfdd65dc44a479397010
Lauren <lauren.a.heavey@gmail.com>
2023-02-07 10:51:45 -0600

    Add background, some login form MUI.

--- src/components/@Dashboard/Dashboard.jsx
+++ src/components/@Dashboard/Dashboard.jsx
@@ -8,102 +8,28 @@ import './Dashboard.css';
 //MAPBOX
 import mapboxgl from '!mapbox-gl'; 
 
-
-
 function Dashboard() {
   const user = useSelector((store) => store.user);
-  const lineCoordinates = useSelector((store) => store.line);
-  // const usableCoordinates = lineCoordinates.map((pair) => Number(Object.values(pair)));
-  const usableCoordinates = (lineCoordinates.map((pair) => Object.values(pair)));
-  // const usableCoordinatesJunior = usableCoordinates.map((pair) => parseInt(pair))
-
-  const dispatch = useDispatch();
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-93.0918);
-  const [lat, setLat] = useState(44.9481);
-  const [zoom, setZoom] = useState(14);
 
   useEffect(() => {
-    dispatch({ type: 'FETCH_LINE'})
-
-    /** ---------- MAPBOX ---------- **/
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/light-v11?',
-      center: [lng, lat],
-      zoom: zoom
+    var map = new mapboxgl.Map({
+      container: 'map',
+      center: [-93.19426931505215, 44.9480407119586],
+      zoom: 10,
+      interactive: false,
+      style: 'mapbox://styles/mapbox/streets-v11'
     });
 
-    // Had to add .current for this to work, found solution in StackOverflow;
-    // Otherwise, map.on was not a function.
-    map.current.on('load', () => {
-      map.current.addSource('lines', {
-        'type': 'geojson',
-        'data': {
-          'type': 'FeatureCollection',
-          'features': [{
-            'type': 'Feature',
-            'properties': {
-              'color': '#33C9EB' // red
-            },
-            'geometry': {
-              'type': 'LineString',
-              'coordinates': usableCoordinates
-              // [
-                // [-93.086713,44.948219],
-                // [-93.087345,44.947911],
-                // [-93.088155,44.947522],
-                // [-93.088719,44.947252],
-                // [-93.090019,44.946589],
-                // [-93.091183,44.946032],
-                // [-93.091282,44.946012],
-                // [-93.092301,44.946146],
-                // [-93.093202,44.946264],
-                // [-93.093226,44.946311],
-                // [-93.093956,44.947111],
-                // [-93.095451,44.948627],
-                // [-93.095805,44.948982],
-                // [-93.096473,44.949649],
-                // [-93.096985,44.950156],
-                // [-93.097036,44.950183],
-                // [-93.097103,44.950285],
-                // [-93.097513,44.950651],
-                // [-93.097836,44.950939],
-                // [-93.097925,44.950975],
-                // [-93.098157,44.951209],
-                // [-93.098287,44.951336],
-                // [-93.098469,44.951509],
-                // [-93.098576,44.951608],
-                // [-93.098608,44.951653],
-                // [-93.098644,44.951718]
-              // ]
-            }
-          }]
-        }
-      });
-      map.current.addLayer({
-        'id': 'lines',
-        'type': 'line',
-        'source': 'lines',
-        'paint': {
-          'line-width': 3,
-          'line-color': ['get', 'color']
-        }
-      });
-    });
-  },[]);
-
-// console.log(lineCoordinates);
-console.log(usableCoordinates);
-// console.log(usableCoordinatesJunior)
+    map.addControl(new mapboxgl.FullscreenControl());
+  })
+  
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
       <div>
-      <div ref={mapContainer} className="map-container" />
+      
+      <div id='map' style={{width: '400px', height: '300px'}}></div>
       </div>
       <LogOutButton className="btn" />
     </div>
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import {useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 function LoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = React.useState(false);
   const errors = useSelector(store => store.errors);
   const dispatch = useDispatch();
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
   const login = (event) => {
     event.preventDefault();
@@ -33,7 +51,25 @@ function LoginForm() {
         </h3>
       )}
       <div>
-        <label htmlFor="username">
+      <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-username" size='small'>Username</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-username"
+            type='text'
+            name="username"
+            required
+            size='small'
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            startAdornment={
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            }
+            label="Username"
+          />
+        </FormControl>
+        {/* <label htmlFor="username">
           Username:
           <input
             type="text"
@@ -42,10 +78,37 @@ function LoginForm() {
             value={username}
             onChange={(event) => setUsername(event.target.value)}
           />
-        </label>
+        </label> */}
       </div>
       <div>
-        <label htmlFor="password">
+      <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password" size='small'>Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            name="password"
+            required
+            size='small'
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+
+
+        {/* <label htmlFor="password">
           Password:
           <input
             type="password"
@@ -54,7 +117,7 @@ function LoginForm() {
             value={password}
             onChange={(event) => setPassword(event.target.value)}
           />
-        </label>
+        </label> */}
       </div>
       <div>
         <input className="btn" type="submit" name="submit" value="Log In" />
